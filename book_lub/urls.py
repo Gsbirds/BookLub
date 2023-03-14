894e2f5e3f283b173096a71bda73ccad08e04762
Your Name <you@example.com>
2023-03-13 23:22:01 -0600

    Added signup, login

--- book_lub/urls.py
+++ book_lub/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('booklub/', include ("books.urls")),
     path("", redirect_to_page, name="home_page"),
+    path("accounts/", include("accounts.urls")),
     
 ]+ static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
 
--- books/views.py
+++ books/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render, redirect
 from .models import File
 from .forms import FileForm
-from django.http import HttpResponse
+import fitz
+from collections import Counter
+
+# def dummy_create_index(received_file):
+#     pass
+
+    ## check if file is a pdf
+
+    ## check if file has been received before (compare file hashes)
+
+    ## if the file has not been received then read all text contents into an array
+
+    ## array_of_words_from_pdf_filtered = [word for word in array_of_words_from_pdf if word not in STOP_WORDS_ARRAY]
+
+    ## counter = Counter(array_of_words_from_pdf_filtered)
+
+    ## most_common_words = counter.most_common(100)
+
+    ## store most_common_words in ArrayField model that ties the list of words to an uploaded file
+
+# def search_dummy(term):
+#     ## return a list of all files that contain the search term(s) with links to interact with them
 
 
 def search_results(request):
