54fb75602996b8aeaa5c207c47a453b826313802
gux <gabbyburgard@yahoo.com>
2023-08-10 16:41:09 -0600

    first

--- book_lub/settings.py
+++ book_lub/settings.py
@@ -114,11 +114,13 @@ USE_I18N = True
 
 USE_TZ = True
 
+import os
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'  # URL for static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
